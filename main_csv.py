3a8015481a67e67fd0cc54d0ddfed62fdc0f6b27
ZacharyLane1204 <porridge4512@gmail.com>
2023-02-06 01:04:10 +1300

    New Google Sheets Modification for csv

--- main_csv.py
+++ main_csv.py
@@ -7,14 +7,24 @@ Created on Thu Jan 26 21:26:05 2023
 
 import numpy as np
 from focus_BC_utils import *
+import pandas as pd
+import requests
 
 # Data that I input. Feel free to adjust as this is a rough guide
 # I will do tests to get more and better data points
 
-data = np.genfromtxt('focusBC.csv', skip_header=1, delimiter = ',')
+sheet_id = "1MC6uRbzfPupTay3QvXSHlgwqOou4a6H2Ac3feXzrVb4"
+sheet_name = "Sheet1"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+URL = "https://docs.google.com/spreadsheets/d/1MC6uRbzfPupTay3QvXSHlgwqOou4a6H2Ac3feXzrVb4/edit#gid=0"
 
-focus = data[:,0]
-FWHM = data[:,1]
+sheets = requests.get(url)
+open('focusBC.csv', 'wb').write(sheets.content)
+
+df = pd.read_csv('focusBC.csv')
+
+focus = df['Focus']
+FWHM = df['FWHM']
 
 focus_function(focus, FWHM)
 
