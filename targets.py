061800722270f0a13d33c085d5189539be112e26
ZacharyLane1204 <porridge4512@gmail.com>
2023-02-10 00:54:16 +1300

    Can search moving targets!

--- targets.py
+++ targets.py
@@ -13,12 +13,15 @@ from utilly import *
 from exposure_BC_utils import bc_focus
 from astroquery.simbad import Simbad
 from planet_position_main import planet_coordinates
-
+from find_asteroid import horizons
+from astropy.time import Time
+import warnings
 
 # Data that I input. Feel free to adjust as this is a rough guide
 # I will do tests to get more and better data points
 
 package_directory = os.path.dirname(os.path.abspath(__file__)) + '/'
+warnings.filterwarnings("ignore")
 
 def get_target_list():
     sheet_id = "1l4JhjWdfvWFeNe-L6eJdcY93oLUa7MS4qk2H5n3-ERY"
@@ -69,35 +72,47 @@ def make_entries(date, data, readout=5):
         name = l['Target Name']
         name = str(name)
         
+        date = str(date)
+        date_l = date[:4] + '-' + date[4:6] + '-' + date[6:]
+        input_time = date_l + ' 13:00:00'
+        date = int(date)
+        
         if name.lower() == 'jupiter':
             print()
             print('Finding coordinates for Jupiter')
             ra, dec = planet_coordinates('Jupiter', date, printout = False)   
+            print('Found coordinates for Jupiter')
         elif name.lower() == 'mercury':
             print()
             print('Finding coordinates for Mercury')
             ra, dec = planet_coordinates('Mercury', date, printout = False)             
+            print('Found coordinates for Mercury')
         elif name.lower() == 'venus':
             print()
             print('Finding coordinates for Venus')
             ra, dec = planet_coordinates('Venus', date, printout = False)
+            print('Found coordinates for Venus')
         elif name.lower() == 'mars':
             print()
             print('Finding coordinates for Mars')
             ra, dec = planet_coordinates('Mars', date, printout = False)
+            print('Found coordinates for Mars')
         elif name.lower() == 'saturn':
             print()
             print('Finding coordinates for Saturn')
             ra, dec = planet_coordinates('Saturn', date, printout = False)  
+            print('Found coordinates for Saturn')
         elif name.lower() == 'uranus':
             print()
             print('Finding coordinates for Uranus')
             ra, dec = planet_coordinates('Uranus', date, printout = False)  
+            print('Found coordinates for Uranus')
         elif name.lower() == 'neptune':
             print()
             print('Finding coordinates for Neptune')
             ra, dec = planet_coordinates('Neptune', date, printout = False) 
-
+            print('Found coordinates for Neptune')
+        
         if name == 'nan':
             continue
         
@@ -108,23 +123,77 @@ def make_entries(date, data, readout=5):
             print()
             print('No exposure time given. Setting "{0}" to {1}s'.format(name, exptime))
         if ra == 'nan':
-            print()
-            print('No RA coordinates entered, searching for "{0}"'.format(name))
-            obj = Simbad.query_object(name)
-    
-            ra = str(obj['RA'][0])
-            dec = str(obj['DEC'][0])
-            ra = coordinates_space(ra)
-            dec = coordinates_space(dec)
+            if name.lower() == 'io':
+                print()
+                print('Checking Horizons for "{0}"'.format(name))
+                mydate = Time(input_time).jd  # pass date as string
+                ra, dec = horizons(501,mydate,474)
+                ra = ra/360*24
+                print('Coordinates found for "{0}"'.format(name))
+            elif name.lower() == 'pluto':
+                print()
+                print('Checking Horizons for "{0}"'.format(name))
+                mydate = Time(input_time).jd  # pass date as string
+                ra, dec = horizons(999,mydate,474)
+                ra = ra/360*24
+                print('Coordinates found for "{0}"'.format(name))
+            else:
+                print()
+                print('No RA coordinates entered, searching for "{0}"'.format(name))
+                try:
+                    obj = Simbad.query_object(name)
+                    ra = str(obj['RA'][0])
+                    dec = str(obj['DEC'][0])
+                    ra = coordinates_space(ra)
+                    dec = coordinates_space(dec)
+                    print('Coordinates found for "{0}"'.format(name))
+                except TypeError: 
+                    print()
+                    print('Checking Horizons for "{0}"'.format(name))
+                    mydate = Time(input_time).jd  # pass date as string
+                    ra, dec = horizons('charon',mydate,474)
+                    ra = ra/360*24
+                    print('Coordinates found for "{0}"'.format(name))
+                except:
+                    print()
+                    print('No target found in SIMBAD or Horizons. Skipping.')
+                    continue
         elif dec == 'nan':
-            print()
-            print('No DEC coordinates entered, searching for "{0}"'.format(name))
-            obj = Simbad.query_object(name)
-    
-            ra = str(obj['RA'][0])
-            dec = str(obj['DEC'][0])
-            ra = coordinates_space(ra)
-            dec = coordinates_space(dec)
+            if name.lower() == 'io':
+                print()
+                print('Checking Horizons for "{0}"'.format(name))
+                mydate = Time(input_time).jd  # pass date as string
+                ra, dec = horizons(501,mydate,474)
+                ra = ra/360*24
+                print('Coordinates found for "{0}"'.format(name))
+            elif name.lower() == 'pluto':
+                print()
+                print('Checking Horizons for "{0}"'.format(name))
+                mydate = Time(input_time).jd  # pass date as string
+                ra, dec = horizons(999,mydate,474)
+                ra = ra/360*24
+                print('Coordinates found for "{0}"'.format(name))
+            else:
+                print()
+                print('No DEC coordinates entered, searching for "{0}"'.format(name))
+                try:
+                    obj = Simbad.query_object(name)
+                    ra = str(obj['RA'][0])
+                    dec = str(obj['DEC'][0])
+                    ra = coordinates_space(ra)
+                    dec = coordinates_space(dec)
+                    print('Coordinates found for "{0}"'.format(name))
+                except TypeError: 
+                    print()
+                    print('Checking Horizons for "{0}"'.format(name))
+                    mydate = Time(input_time).jd  # pass date as string
+                    ra, dec = horizons('charon',mydate,474)
+                    ra = ra/360*24
+                    print('Coordinates found for "{0}"'.format(name))
+                except:
+                    print()
+                    print('No target found in SIMBAD or Horizons. Skipping.')
+                    continue
         else:
             ra = coordinates_angle(ra)
             dec = coordinates_angle(dec)
